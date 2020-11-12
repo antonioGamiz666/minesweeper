5981987a84aef2e0b8228d6ae8ec9145693c97a7
David cadevall <david.cadevall@e-campus.uab.cat>
2020-11-12 12:38:34 +0100

    Moved the main to controller. Implemented translation matrix int->String

--- src/main/java/minesweeper/GameController.java
+++ src/main/java/minesweeper/GameController.java
@@ -1,5 +1,17 @@
 package minesweeper;
+import java.util.Scanner;
 
 public class GameController {
+	
+	Scanner input = new Scanner(System.in);
+	
+	public int write() {
+		int number = input.nextInt();
+		return number;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
 
 }
--- src/main/java/minesweeper/GameView.java
+++ src/main/java/minesweeper/GameView.java
@@ -12,7 +12,7 @@ public static String[][] field = new String[9][9];
 
 private static String closedcell = "[]";
 private static String openedcell_empty = "[_]";
-private static String openeddcell_mine = "[X]";
+private static String openedcell_mine = "[X]";
 private static String number1 = "[1]";
 private static String number2 = "[2]";
 private static String number3 = "[3]";
@@ -26,18 +26,24 @@ private static String flag = "[!]";
 
 
 	
-	public static void main(String[] args) {
+
 		
 		//while no se acabe el juego
-		//preguntar si quiere abrir una casilla o poner flag
 		//preguntar posicion -> meter posicion por teclado 
-		
-		for ( int i = 0; i <= 8; i++) {
-			for ( int j = 0; j <= 8; j++) {
-				field[i][j]=closedcell;
-			}
 
+		
+		
+		 
+		public void askPosX() {			
+			System.out.println("Introduce posX: ");			
 		}
+		
+		public void askPosY() {
+			System.out.println("Introduce posY: ");
+		}
+		
+		public void printMatrix(int [][]userBoard) {
+		
 		
 		for ( int i = 0; i <= 8; i++) {
 			for ( int j = 0; j <= 8; j++) {
@@ -52,7 +58,45 @@ private static String flag = "[!]";
 				}
 				else {
 					if (j == 0) System.out.print(+i);
-					else System.out.print("\t"+field[i][j]);
+					else {
+						switch(userBoard[i][j]) {
+							case -1:
+								this.field[i][j]=this.closedcell;
+								break;
+							case 0:
+								this.field[i][j]=this.openedcell_empty;
+								break;
+							case 1:
+								this.field[i][j]=this.number1;
+								break;
+							case 2:
+								this.field[i][j]=this.number2;
+								break;
+							case 3:
+								this.field[i][j]=this.number3;
+								break;
+							case 4:
+								this.field[i][j]=this.number4;
+								break;
+							case 5:
+								this.field[i][j]=this.number5;
+								break;
+							case 6:
+								this.field[i][j]=this.number6;
+								break;
+							case 7:
+								this.field[i][j]=this.number7;
+								break;
+							case 8:
+								this.field[i][j]=this.number8;
+								break;
+							case 9:
+								this.field[i][j]=this.openedcell_mine;
+								break;
+								
+						}
+						System.out.print("\t"+field[i][j]);
+					}
 					
 				}
 				
@@ -61,5 +105,6 @@ private static String flag = "[!]";
 			
 		}
 
+	
 	}
 }
