fac4d9ff2184327e289bbc49ce1c1a31f88f8e3d
David cadevall <david.cadevall@e-campus.uab.cat>
2020-11-15 12:47:02 +0100

    Changed methods to static to run main
    with ruben

--- src/main/java/minesweeper/GameController.java
+++ src/main/java/minesweeper/GameController.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 
 public class GameController {
 
-    public int positionX=0;
-    public int positionY=0;
+//    public int positionX=0;
+//    public int positionY=0;
 
 //    public int write() {
 //        Scanner input = new Scanner(System.in);
@@ -14,7 +14,7 @@ public class GameController {
 //    }
 
 
-    public void requestPosX(GameView gameview, InputKeyboard input) {
+    public static int requestPosX(GameView gameview, InputKeyboard input) {
         //Requests the view to ask for new positionX
         int aux=-1;
         while(aux > 7 || aux < 0) {
@@ -23,44 +23,48 @@ public class GameController {
             aux=input.read();
 
         }
-        this.positionX=aux;
+         return aux;
     }
 
-    public void requestPosY(GameView gameview, InputKeyboard input) {
+    public static int requestPosY(GameView gameview, InputKeyboard input) {
         //Requests the view to ask for new positionY
         int aux=-1;
         while(aux > 7 || aux < 0) {
             gameview.askPosY();
             aux=input.read();
         }
-        this.positionY=aux;
+        return aux;
     }
 
-    public void UpdateModel(int posX, int posY, Board board) {
+    public static void UpdateModel(int posX, int posY, Board board) {
         //Updates the game by sending those positions to the Model
         //Calling openCell with the new positions?
         board.openCell(posX, posY);
 
     }
 
-    public void UpdateView(GameView gameview, Board board) {
+    public static void UpdateView(GameView gameview, Board board) {
         //Telling the view to print the new matrix
         //Calling the printMatrix from the view?
         gameview.printMatrix(board.getBoardUser());
     }
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
         Board game = new Board();
         GameView gv = new GameView();
         InputKeyboard input = new InputKeyboard();
+        
+        int positionX=0;
+        int positionY=0;
+        
 
         while(game.lose==false || game.win==false) {
-            this.UpdateView(gv,game);
-            this.requestPosX(gv, input);
-            this.requestPosY(gv, input);
-            this.UpdateModel(this.positionX, this.positionY, game);
+            UpdateView(gv,game);
+            positionX=requestPosX(gv, input);
+            positionY=requestPosY(gv, input);
+            UpdateModel(positionX, positionY, game);
         }
-        this.UpdateView(gv,game);
+        UpdateView(gv,game);
         gv.gameOver();
     }
 
